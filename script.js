79de6161e018e4447a3128ce406c1f9d34dcaa94
Abhishek <abhishek.bisht54@yahoo.com>
2022-02-05 00:52:11 +0530

    Press Shift to generate the Ticket and Dynamically creating and appending tickes

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 let addbtn = document.querySelector(".add-btn");
 let addFlag = false;
 let modalCont = document.querySelector(".modal-cont");
+let mainCont = document.querySelector(".main-cont")
 
 
 
@@ -19,3 +20,29 @@ addbtn.addEventListener("click",(e) => {
 		modalCont.style.display = "none";
 	}
 })
+
+
+// To pressing Shift Ticket will be generated
+modalCont.addEventListener("keydown", (e) => {
+    let key = e.key;
+    if (key === "Shift") {
+        createTicket(modalPriorityColor, textareaCont.value);
+        addFlag = false;
+        setModalToDefault();
+    }
+})
+
+// Repetitive work of creating ticket Handled
+function createTicket(){
+	let ticketCont = document.createElement("div");
+	ticketCont.setAttribute("class", "ticket-cont");
+	ticketCont.innerHTML  = `  <div class="ticket-color ${ticketColor}"></div>
+        <div class="ticket-id">#${id}</div>
+        <div class="task-area">${ticketTask}</div>
+        <div class="ticket-lock">
+            <i class="fas fa-lock"></i>
+        </div>`;
+	mainCont.appendChild(ticketCont);
+
+
+}
